4d595a91200d6a074629a45326cf2f4991c288d9
niamx <muhammadniam1999@gmail.com>
2023-05-16 23:20:57 +0700

    create new middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'adminAccess' => \App\Http\Middleware\AdminAccess::class,
+        'make' => \App\Http\Middleware\MakeMiddleware::class,
     ];
 }
